1ddd5e97ddf6f572d03890041cda4e681cd2fa62
eduardo hernandez <eduardohralejandro@gmail.com>
2018-07-13 10:30:52 +0200

    last question completed

--- js/todo.js
+++ js/todo.js
@@ -1,3 +1,4 @@
+
 let TODOS = [];
 
 function update(){
@@ -37,22 +38,38 @@ function update(){
 
         //// ITEM COUNTER
 
-        const items = TODOS.filter(someItems => !someItems.done);       
+        const items = TODOS.filter(someItems =>  !someItems.done);       
         const $count = document.querySelector('.todo-count');
         items.length === 1 ? $count.textContent = `${items.length} item left` : $count.textContent = `${items.length} items left`;        
-        
+        // BIND THING 
+        $button.addEventListener('click', onDeleteItem.bind(null, item.id));
+
         //this solution is shorter but it would not be readable for everyone. I like to do it like this, the first one is too long in my opinion.        
 
         // const x = TODOS.filter(I =>!I.done);
         // const c = document.querySelector('.todo-count');
         // x === 1 ? $c.textContent = `${x.length} item left` : $c.textContent = `${x.length} items left`;
-
-
+        // const deleteItem = document.get
+        // $button.addEventListener('click', onDeleteItem);s
 
     }
     document.querySelector(".main").style.display = "block";
 }
 
+function onDeleteItem(id){
+     TODOS = TODOS.filter(removeItems => removeItems.id !== id);
+    ///////////////////// but I still have 1 item left, this is not working well :/ I just pasted your code, it's almost the same as toggle to see the difference
+     // second I tried this  but It did almost the same thing 
+    // $button.addEventListener('click', onDeleteItem);
+
+        // function onDeleteItem(e){
+        //     e.target.parentElement.remove()
+  
+        // }
+             //https://www.youtube.com/watch?v=GhbhD1HR5vk I found this video to understand a little bit more
+
+    update();
+}
 
 function onToggleTodo(id){
   const todo = TODOS.find(todo => todo.id === id);
@@ -61,14 +78,8 @@ function onToggleTodo(id){
   update();
 }
 
-// function onDeleteItem(id){
-    
-//     const deleteElement = TODOS.filter(deleteSomeItems => deleteItems.id)
-// } still working on this
-
 function onNewTodo(e){
     const title = e.target.value;
-    console.log(title);
     //same as this line :
     //const title = document.querySelector('.new-todo').value;
 
@@ -83,13 +94,10 @@ function onNewTodo(e){
     e.target.value = "";
    
 }
+
+
+
 // Select the new todo input input field
 const $newTodo = document.querySelector(".new-todo");
 $newTodo.addEventListener('change', onNewTodo);
-
-// const $deleteItem = document.querySelector(".destroy");
-// $deleteItem.addEventListener('click', onDeleteItem); still working on this
-
-
-
 
